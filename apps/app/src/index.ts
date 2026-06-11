ce498f8c2df01030e95c3bc76281c20fb1836b1d
Mateusz Tkacz <mateusz.tkacz94.dev@gmail.com>
2026-06-11 12:25:40 +0200

    chore: test

--- apps/app/src/index.ts
+++ apps/app/src/index.ts
@@ -1,5 +1,5 @@
 import { greet } from '@tmp-test-changesets/public-lib'
 import { greetLoudly } from '@tmp-test-changesets/private-lib'
 
-console.log(greet('world'))
+console.log(greet('world!'))
 console.log(greetLoudly('world'))
